01dfa00b134edb262fe9088928ebeb5cc4b932e5
haipeng fan <fanhaipeng0403@gmail.com>
2024-03-02 14:27:49 +0800

    update

--- scripts/keys.sh
+++ scripts/keys.sh
@@ -3,4 +3,3 @@ cd ~/.ssh
 ssh-keygen -t rsa -f id_rsa
 ssh-copy-id -i id_rsa.pub postgres@postgres1.local
 ssh-copy-id -i id_rsa.pub postgres@postgres2.local
-ssh-copy-id -i id_rsa.pub postgres@postgres3.local
