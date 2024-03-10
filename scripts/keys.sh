76510e702e55371ad09e9b7ac67ead7f13a8c892
haipeng fan <fanhaipeng0403@gmail.com>
2024-03-10 20:23:08 +0800

    update

--- scripts/keys.sh
+++ scripts/keys.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+mkdir ~/.ssh
+chmod 700 ~/.ssh
 cd ~/.ssh
-ssh-keygen -t rsa -f id_rsa
-ssh-copy-id -i id_rsa.pub postgres@postgres1.local
-ssh-copy-id -i id_rsa.pub postgres@postgres2.local
+ssh-keygen -t rsa -f id_rsa_pgpool
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres1.local
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres2.local
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres3.local
+
+su - postgres
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+cd ~/.ssh
+ssh-keygen -t rsa -f id_rsa_pgpool
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres1.local
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres2.local
+ssh-copy-id -i id_rsa_pgpool.pub postgres@postgres3.local
