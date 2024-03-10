7f701e665a4accbf6ff242388bf1f36dd032c4b7
haipeng fan <fanhaipeng0403@gmail.com>
2024-03-10 16:23:32 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ sudo yum install epel-release -y
 sudo yum install ansible -y
 ansible-galaxy collection install community.general
 ansible-playbook generate-key-use-password.yml  -i inventory.ini
-ansible-playbook postgresql14_install.yml -i inventory.ini
+ansible-playbook main.yml -i inventory.ini
